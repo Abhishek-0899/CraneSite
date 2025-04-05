1888b51f28c081d592ce1068b33984848cbd6c88
Abhishek Kumar Pandit <akp310899@gmail.com>
2025-04-06 01:44:00 +0530

    app changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Rental from "./components/rental";
-import Service from "./components/service";
-import Team from "./components/team";
+
 
 function App() {
   return (
     <>
       <Navbar />
-      <Service />
-      <Rental/>
-      <Team/>
     </>
   );
 }
